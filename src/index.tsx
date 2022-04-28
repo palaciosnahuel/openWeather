9af8d586713f75993d572928b1eb45eaac3495cb
npalacios <npalacios@edrans.com>
2022-04-28 19:49:50 -0300

    Added React Query dependency

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './chakratheme'
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const queryClient = new QueryClient();
+
 root.render(
+  <QueryClientProvider client={queryClient}>
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
+  </QueryClientProvider>
+
 );
