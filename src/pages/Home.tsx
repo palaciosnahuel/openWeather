72a8b7ece0a8f2f5121c23bd2bb93ce4559be4e2
npalacios <npalacios@edrans.com>
2022-04-28 20:48:34 -0300

    Consuming api forecast in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,27 +3,27 @@ import React from 'react'
 import CurrentWeatherCard from '../components/CurrentWeatherCard/CurrentWeatherCard'
 import ForecastItemCard from '../components/ForecastItemCard/ForecastItemCard'
 import { useQuery } from 'react-query';
-import { getCurrentWeather } from '../api/openWeather/openWeather';
+import { getCurrentWeather, getForecast } from '../api/openWeather/openWeather';
+import Loader from '../components/Loader/Loader';
 
 const Home = () => {
 
-  
-  const {data, isFetched} = useQuery(["getWeather"], () => getCurrentWeather("Buenos Aires"))
+  const {data : weatherData, isFetched: weatherIsFetched } = useQuery(["getWeather"], () => getCurrentWeather("Buenos Aires"))
+  const {data : forecastData, isFetched: forecastIsFetched } = useQuery(["getForecast"], () => getForecast("Buenos Aires"))
 
   return (
     <Box p={10} mt={"5%"} mx={"20%"} bg={"red.300"} rounded="2xl">
       <Flex justifyContent={"center"}>
-        {isFetched && <CurrentWeatherCard data={data!} />} 
+        {weatherIsFetched ? <CurrentWeatherCard data={weatherData!} /> : <Loader/> } 
       </Flex>
-      
       <Flex direction={'row'} justifyContent={"center"} wrap={"wrap"}>
-        <ForecastItemCard/>
-        <ForecastItemCard/>
-        <ForecastItemCard/>
-        <ForecastItemCard/>
-        <ForecastItemCard/>
+        {
+          forecastIsFetched ? forecastData!.list.map( (data)=> {
+            data.timezone = forecastData!.city.timezone; // Add Timezone to data Object.
+            return (<ForecastItemCard data={data}/>)
+          }) : <Loader/> 
+        }
       </Flex>
-     
     </Box>
   )
 }
