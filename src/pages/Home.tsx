c807cc73b6b0cee374d631d1fa97f0b7b1fe89e9
npalacios <npalacios@edrans.com>
2022-04-27 23:32:27 -0300

    Edit Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import { Container } from '@chakra-ui/react'
 import React from 'react'
-import { Text } from '@chakra-ui/react'
+import CurrentWeatherCard from '../components/CurrentWeatherCard/CurrentWeatherCard'
 
 const Home = () => {
   return (
-    <Text>Home Page</Text>
+    <Container>
+      <CurrentWeatherCard/>
+    </Container>
   )
 }
 
