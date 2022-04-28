d811bae81bba37c2657242993c364d840cc140fe
npalacios <npalacios@edrans.com>
2022-04-28 20:48:03 -0300

    ForecastItem consuming api

--- src/components/ForecastItemCard/ForecastItemCard.tsx
+++ src/components/ForecastItemCard/ForecastItemCard.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Center,Box,Text,Flex,Image } from '@chakra-ui/react'
+import { Box,Text,Flex,Image } from '@chakra-ui/react'
+import { ForecastItem } from '../../api/openWeather/types';
+import { parseDateTime } from '../../utils/DateUtils';
 
-const ForecastItemCard = () => {
+interface ForecastItemCardProps {
+  data: ForecastItem;
+}
+
+
+
+const ForecastItemCard : React.FC<ForecastItemCardProps> = ({data}) => {
+
+  const date = parseDateTime(data.dt,data.timezone);
+
   return (
     <>
-    <Flex bgGradient='linear(to-r, gray.100, gray.300)' margin={2} w={155} direction={'column'} align={"center"} rounded={"lg"}>
+    <Flex bgGradient='linear(to-r, gray.100, gray.300)' margin={2} w={165} direction={'column'} align={"center"} rounded={"lg"}>
       <Box p={5}>
-      <Text fontWeight={400} as='i' fontSize='md'>{"Lluvias"}</Text>
-      <Text>{"99.99"} / {"99.99"}</Text>
-      <Image src={`http://openweathermap.org/img/wn/10d@2x.png`}  w={100} alt={"Complete"} />
-      <Text>23/04</Text>
+      <Text align={"center"}>{data.temp.max} / {data.temp.min}</Text>
+      <Text align={"center"} fontWeight={700}  fontSize='xs'>{data.weather[0].description}</Text>
+      <Image src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}  w={100} alt={"Complete"} />
+      <Text align={"center"}>{date.getDate() + "/"+ date.getMonth()}</Text>
       </Box>
     </Flex>
   </> 
