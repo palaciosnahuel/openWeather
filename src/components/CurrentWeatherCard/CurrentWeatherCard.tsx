6c8c9c9c7a18dbad1ad1adbecda2024a92994bd4
npalacios <npalacios@edrans.com>
2022-04-28 23:51:07 -0300

    restyle

--- src/components/CurrentWeatherCard/CurrentWeatherCard.tsx
+++ src/components/CurrentWeatherCard/CurrentWeatherCard.tsx
@@ -1,30 +1,42 @@
 import React from 'react'
-import { Box,Flex,Image } from '@chakra-ui/react'
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import { CurrentWeather } from '../../api/openWeather/types';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDroplet, faArrowDown, faArrowUp, faGauge, faTemperature0 } from '@fortawesome/free-solid-svg-icons'
 interface CurrentWeatherCardProps {
   data: CurrentWeather;
 }
 
-const CurrentWeatherCard: React.FC<CurrentWeatherCardProps> = ({data}) => {
+const CurrentWeatherCard: React.FC<CurrentWeatherCardProps> = ({ data }) => {
   return (
-    <Flex  wrap={"wrap"}  justifyContent={"center"} bgGradient='linear(to-r, gray.100, gray.300)' p={5} borderRadius={5} my={1}>
+    <Flex wrap={"wrap"}  bgGradient='linear(to-r, gray.100, gray.300)' p={5} borderRadius={5} my={1}>
       <Flex direction={'column'} mx={10} alignItems={"center"}>
-        {data.name}
+        <Text fontWeight={700} fontSize={"xl"}>{data.name}</Text>
         <Flex alignItems={"center"}>
-        <Image w={"100px"} h={"100px"} src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
-        <Box>{data.main.temp}</Box>
+          <Image w={"100px"} h={"100px"} src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
+          <Box fontWeight={700} fontSize={"xx-large"}>{`${Math.round(data.main.temp)}°`}</Box>
         </Flex>
-        {data.weather[0].description}
+        <Text fontWeight={700}>
+         {data.weather[0].description}
+        </Text>
       </Flex>
-      <Flex direction={'column'} justifyContent={"center"} mx={10} >
-        <Box>{`ST ${data.main.feels_like}`}</Box>
+      <Flex direction={'column'} justifyContent={"space-evenly"} mx={10} >
+        <Box>
+          <FontAwesomeIcon icon={faTemperature0} />
+          {` ${Math.round(data.main.feels_like)}°`}
+        </Box>
         <Flex>
-          <div>{`MX ${data.main.temp_max}`}</div>
-          <div>{`MN ${data.main.temp_min}`}</div>
+          <Text px={1}>
+            <FontAwesomeIcon icon={faArrowUp} />
+            {` ${data.main.temp_max}°`}
+          </Text>
+          <Text px={1}>
+            <FontAwesomeIcon icon={faArrowDown} />
+            {` ${data.main.temp_min}°`}
+          </Text>
         </Flex>
-        <div>{`HMDY ${data.main.humidity}%`}</div>
-        <div>{`PSRE ${data.main.pressure}hpca`}</div>
+        <div><FontAwesomeIcon icon={faDroplet} />{` ${data.main.humidity}%`}</div>
+        <div><FontAwesomeIcon icon={faGauge} />{` ${data.main.pressure} hpca`}</div>
       </Flex>
     </Flex>
   )
