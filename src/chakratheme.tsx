7248a84b7a1976f597640cf0cb05649a8cfc2e6d
npalacios <npalacios@edrans.com>
2022-04-28 23:51:20 -0300

    Change themefont

--- src/chakratheme.tsx
+++ src/chakratheme.tsx
@@ -3,6 +3,6 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   fonts: {
     heading: 'Open Sans, sans-serif',
-    body: 'Raleway, sans-serif',
+    body: 'Roboto, sans-serif',
   },
 })
