75d0db8f278d16dfde49058b6e482f0b4cd62819
npalacios <npalacios@edrans.com>
2022-04-30 18:10:13 -0300

    Add lat long posibility in request

--- src/api/openWeather/openWeather.tsx
+++ src/api/openWeather/openWeather.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CurrentWeather , Forecast } from './types';
+import { CurrentWeather , Forecast ,GeolocationParameters} from './types';
 
 
 // Api Configs.
@@ -8,15 +8,29 @@ const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY
 const COUNT = 5
 
 // Api DOC https://openweathermap.org/forecast16
-export const getForecast = async (city:string) => {
-  let url = `${URL}/forecast/daily?q=${city}&appid=${apiKey}&units=metric&cnt=${COUNT}`;
+export const getForecast = async (city:(string | GeolocationParameters)) => {
+  let url
+  if(typeof city === "string"){ 
+    url = `${URL}/forecast/daily?q=${city}&appid=${apiKey}&units=metric&cnt=${COUNT}`
+  }
+  else{
+    url = `${URL}/forecast/daily?lat=${city.lat}&lon=${city.long}&appid=${apiKey}&units=metric&cnt=${COUNT}`
+  }
   const {data} = await axios.get<Forecast>(url);
   return data;
 };
 
 // API DOC https://openweathermap.org/current
-export const getCurrentWeather = async (city:string) => {
-  let url = `${URL}/weather?q=${city}&appid=${apiKey}&units=metric`;
+export const getCurrentWeather = async (city:string | GeolocationParameters) => {
+  
+  let url
+  if(typeof city === "string"){ 
+    url = `${URL}/weather?q=${city}&appid=${apiKey}&units=metric`;
+  }
+  else{
+    url = `${URL}/weather?lat=${city.lat}&lon=${city.long}&appid=${apiKey}&units=metric&cnt=${COUNT}`
+  }
+
   const {data} = await axios.get<CurrentWeather>(url);
   return data;
 };
